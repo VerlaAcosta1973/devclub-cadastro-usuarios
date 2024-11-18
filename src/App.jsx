b0823f0b3e051245729d7e5d62a43f531f72bcf2
VerlaAcosta1973 <verlaacostaramos1973@gmail.com>
2024-11-17 23:15:03 -0300

    Terminado o layout da aplicação

---
+++ src/App.jsx
@@ -0,0 +1,71 @@
+import { useRef } from "react";
+
+import {
+  Title,
+  Container,
+  Form,
+  TopBackground,
+  ContainerInputs,
+  Input,
+  Button,
+  InputLabel,
+} from "./styles";
+
+import UsersImage from "./assets/users.png";
+
+function Home() {
+  const inputName = useRef();
+  const inputAge = useRef();
+  const inputEmail = useRef();
+
+  function registerNewUser() {
+    console.log(inputName.current.value);
+  }
+
+  return (
+    <Container>
+      <TopBackground>
+        <img src={UsersImage} alt="imagem-usuários" />
+      </TopBackground>
+
+      <Form action="">
+        <Title>Cadastrar Usuário</Title>
+
+        <ContainerInputs>
+          <div>
+            <InputLabel>
+              Nome<span> *</span>
+            </InputLabel>
+            <Input type="text" placeholder="Nome do usuário" ref={inputName} />
+          </div>
+          <div>
+            <InputLabel>
+              Idade<span> *</span>
+            </InputLabel>
+            <Input
+              type="number"
+              placeholder="Idade do usuário"
+              ref={inputAge}
+            />
+          </div>
+        </ContainerInputs>
+        <div style={{ width: "100%" }}>
+          <InputLabel>
+            Email<span> *</span>
+          </InputLabel>
+          <Input
+            type="email"
+            placeholder="E-mail do usuário"
+            ref={inputEmail}
+          />
+        </div>
+
+        <Button type="button" onClick={registerNewUser}>
+          Cadastrar Usuário
+        </Button>
+      </Form>
+    </Container>
+  );
+}
+
+export default Home;
